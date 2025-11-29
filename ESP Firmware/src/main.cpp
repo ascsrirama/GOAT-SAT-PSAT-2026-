954a692fd7c0890367c2abf90438eb778df401e2
dayzechen <daisychennnz@gmail.com>
2025-11-30 12:11:37 +1300

    Testing GPS again, trying to set up with dashboard

--- ESP Firmware/src/main.cpp
+++ ESP Firmware/src/main.cpp
@@ -1,16 +1,14 @@
 #include <Arduino.h>
 #include "gps.h"
-#include "LoRa.h"
+//#include "LoRa.h"
 
 
 void setup(){
 Serial.begin(115200);
 setupGPS();
-setupLoRa();
 }
 
 void loop(){
   readGPSData();
-  loopLoRa();
 }
 
