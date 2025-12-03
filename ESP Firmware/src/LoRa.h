d9b304b8304b60e682a9b495e1b226cd0ecbadf1
ascsrirama <sanu530@aucklanduni.ac.nz>
2025-12-03 21:07:50 +1300

    quick build test

--- ESP Firmware/src/LoRa.h
+++ ESP Firmware/src/LoRa.h
@@ -1,8 +1,10 @@
 #pragma once
 // put library
-#define LORA_AT_WIOE5
+//#define LORA_AT_WIOE5
+#include <Arduino.h>
 // #include <SoftwareSerial.h>
 // #include <LoRa_AT.h>
 
 void setupLoRa();
 void loopLoRa();
+void sendLoRaLine(const char* line);
